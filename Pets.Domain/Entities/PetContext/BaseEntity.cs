00caa072656ca47bca6ceaab1829ce60329dc7a6
Renzo Alves <djenrolandy@gmail.com>
2023-07-24 17:24:00 -0300

    FIX <VALIDATIONS>: Inclusão de Interface para validação do Name

--- Pets.Domain/Entities/PetContext/BaseEntity.cs
+++ Pets.Domain/Entities/PetContext/BaseEntity.cs
@@ -1,9 +1,10 @@
 using System;
 using Pets.Domain.ValueObjects;
+using Pets.Domain.Validations.Iterfaces;
 
 namespace Pets.Domain.Etities.PetContext
 {
-    public abstract class BaseEntity
+    public abstract class BaseEntity : IValidations
     {
         protected BaseEntity(Name name)
         {
@@ -15,6 +16,8 @@ namespace Pets.Domain.Etities.PetContext
         public Guid Id { get; private set; }
         public Name Name { get; private set; }
         public DateTime CreateDate { get; private set; }
+
+        public abstract bool Validation();
     }
 
 }
